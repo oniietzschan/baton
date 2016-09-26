570e110fda221c85548e40cbbaf1d866f8468a1d
Andrew Minnich <aminnich3@gmail.com>
2016-09-25 23:14:27 -0400

    Separate deadzone setting

--- baton.lua
+++ baton.lua
@@ -1,3 +1,34 @@
+local baton = {
+  _VERSION = 'baton',
+  _DESCRIPTION = 'Input library for LÖVE.',
+  _URL = 'https://github.com/tesselode/baton',
+  _LICENSE = [[
+    MIT License
+
+    Copyright (c) 2016 Andrew Minnich
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+  ]]
+}
+
+
+
 local function length(x, y)
   return (x^2 + y^2)^.5
 end
@@ -10,10 +41,6 @@ local function trim(x, y, max)
   end
   return x, y
 end
-
-
-
-local baton = {}
 
 
 
@@ -152,7 +179,13 @@ function Player:_processPair(pair)
     pair._value = {x = x._binary, y = y._binary}
   else
     local v = {x = x._analog, y = y._analog}
-    pair._value = length(v.x, v.y) > self.deadzone and v or {x = 0, y = 0}
+    if pair.axes.separateDeadzone then
+      v.x = math.abs(v.x) > self.deadzone and v.x or 0
+      v.y = math.abs(v.y) > self.deadzone and v.y or 0
+    else
+      v = length(v.x, v.y) > self.deadzone and v or {x = 0, y = 0}
+    end
+    pair._value = v
   end
   pair._value.x, pair._value.y = trim(pair._value.x, pair._value.y, 1)
 end
