7c7deafa658a695d1fc3b762ae815361c7246efd
Andrew Minnich <aminnich3@gmail.com>
2016-09-25 22:28:20 -0400

    Code cleanup

--- baton.lua
+++ baton.lua
@@ -1,4 +1,21 @@
+local function length(x, y)
+  return (x^2 + y^2)^.5
+end
+
+local function trim(x, y, max)
+  local l = length(x, y)
+  if l > max then
+    x = x / l
+    y = y / l
+  end
+  return x, y
+end
+
+
+
 local baton = {}
+
+
 
 local sourceFunction = {}
 
@@ -48,26 +65,42 @@ function sourceFunction.button(button)
   end
 end
 
+
+
 local Player = {}
+
+-- implementation functions
+
+function Player:_init(controls, joystick)
+  self.inputs = {}
+  self.axes = {}
+  self.pairs = {}
+  self.joystick = joystick
+  self.deadzone = .5
+  self:_updateControls(controls)
+end
+
+local function addSources(input, sources)
+  input._binarySources = {}
+  input._analogSources = {}
+  for i = 1, #sources do
+    local source = sources[i]
+    local type, value = source:match '(.*):(.*)'
+    assert(sourceFunction[type], type .. 'is not a valid input source')
+    if type == 'axis' then
+      table.insert(input._analogSources, sourceFunction[type](value))
+    else
+      table.insert(input._binarySources, sourceFunction[type](value))
+    end
+  end
+end
 
 function Player:_updateControls(controls)
   for name, sources in pairs(controls.inputs) do
     if not self.inputs[name] then
       self.inputs[name] = {value = 0}
     end
-    local input = self.inputs[name]
-    input._binarySources = {}
-    input._analogSources = {}
-    for i = 1, #sources do
-      local source = sources[i]
-      local type, value = source:match '(.*):(.*)'
-      assert(sourceFunction[type], type .. 'is not a valid input source')
-      if type == 'axis' then
-        table.insert(input._analogSources, sourceFunction[type](value))
-      else
-        table.insert(input._binarySources, sourceFunction[type](value))
-      end
-    end
+    addSources(self.inputs[name], sources)
   end
   for name, inputs in pairs(controls.axes) do
     self.axes[name] = {
@@ -83,64 +116,62 @@ function Player:_updateControls(controls)
   end
 end
 
-function Player:_updateInput(input)
-  input._binary = 0
+function Player:_getBinarySources(input)
   for i = 1, #input._binarySources do
     if input._binarySources[i](self) == 1 then
-      input._binary = 1
-      break
+      return 1
     end
   end
+  return 0
+end
 
-  input._analog = 0
+function Player:_getAnalogSources(input)
+  local v = 0
   for i = 1, #input._analogSources do
-    input._analog = input._analog + input._analogSources[i](self)
+    v = v + input._analogSources[i](self)
   end
-  if input._analog > 1 then input._analog = 1 end
+  if v > 1 then v = 1 end
+  return v
+end
 
+function Player:_processInput(input)
+  input._binary = self:_getBinarySources(input)
+  input._analog = self:_getAnalogSources(input)
   local v = math.max(input._binary, input._analog)
   input._value = v > self.deadzone and v or 0
 end
 
-function Player:_updateAxis(axis)
+function Player:_processAxis(axis)
   local n = self.inputs[axis.inputs.negative]
   local p = self.inputs[axis.inputs.positive]
+  axis._usingBinary = n._binary == 1 or p._binary == 1
   axis._binary = p._binary - n._binary
   axis._analog = p._analog - n._analog
-  if axis._binary == 0 then
-    axis._value = math.abs(axis._analog) > self.deadzone and axis._analog or 0
-  else
+  if axis._usingBinary then
     axis._value = axis._binary
+  else
+    axis._value = math.abs(axis._analog) > self.deadzone and axis._analog or 0
   end
 end
 
-function Player:_updatePair(pair)
+function Player:_processPair(pair)
   local x = self.axes[pair.axes.x]
   local y = self.axes[pair.axes.y]
-  if x._binary == 0 and y._binary == 0 then
-    local value = {x = x._analog, y = y._analog}
-    local length = (value.x^2 + value.y^2)^.5
-    pair._value = length > self.deadzone and value or {x = 0, y = 0}
-  else
+  if x._usingBinary or y._usingBinary then
     pair._value = {x = x._binary, y = y._binary}
+  else
+    local v = {x = x._analog, y = y._analog}
+    pair._value = length(v.x, v.y) > self.deadzone and v or {x = 0, y = 0}
   end
-  local length = (pair._value.x^2 + pair._value.y^2)^.5
-  if length > 1 then
-    pair._value.x = pair._value.x / length
-    pair._value.y = pair._value.y / length
-  end
+  pair._value.x, pair._value.y = trim(pair._value.x, pair._value.y, 1)
 end
 
+--public API
+
 function Player:update()
-  for _, input in pairs(self.inputs) do
-    self:_updateInput(input)
-  end
-  for _, axis in pairs(self.axes) do
-    self:_updateAxis(axis)
-  end
-  for _, pair in pairs(self.pairs) do
-    self:_updatePair(pair)
-  end
+  for _, input in pairs(self.inputs) do self:_processInput(input) end
+  for _, axis in pairs(self.axes) do self:_processAxis(axis) end
+  for _, pair in pairs(self.pairs) do self:_processPair(pair) end
 end
 
 function Player:get(name)
@@ -154,19 +185,12 @@ function Player:get(name)
   assert(false, 'No input, axis, or pair found with name ' .. name)
 end
 
-function Player:_init(controls, joystick)
-  self.inputs = {}
-  self.axes = {}
-  self.pairs = {}
-  self.joystick = joystick
-  self.deadzone = .5
-  self:_updateControls(controls)
-end
-
 function baton.newPlayer(...)
   local player = setmetatable({}, {__index = Player})
   player:_init(...)
   return player
 end
+
+
 
 return baton
